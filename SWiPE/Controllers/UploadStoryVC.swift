75ac2892a7343c73437db99522b6e872ac40b162
Zoezoe0717 <aaa591129@gmail.com>
2022-10-30 14:21:12 +0800

    add the function to select album photos

---
+++ SWiPE/Controllers/UploadStoryVC.swift
@@ -0,0 +1,110 @@
+//
+//  updateStoryVC.swift
+//  SWiPE
+//
+//  Created by Zoe on 2022/10/29.
+//
+
+import UIKit
+import AVKit
+import PhotosUI
+
+class UploadStoryVC: UIViewController {
+
+    @IBOutlet weak var showStoryView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func uploadStory(_ sender: Any) {
+        var configuration = PHPickerConfiguration()
+        configuration.selectionLimit = 1
+        configuration.filter = .any(of: [.videos, .images])
+        configuration.preferredAssetRepresentationMode = .current
+        
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+}
+
+//MARK: - PHPickerViewControllerDelegate
+extension UploadStoryVC: PHPickerViewControllerDelegate {
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+        
+        print(Thread.isMainThread)
+        
+        guard let result = results.first else { return }
+        let assetid = result.assetIdentifier
+        
+        print(assetid as Any)
+        
+        let provider = result.itemProvider
+        let types = provider.registeredTypeIdentifiers
+        print("types: \(types)")
+        
+        if provider.hasItemConformingToTypeIdentifier(UTType.movie.identifier) {
+            self.dealWithVideo(result)
+        } else if provider.canLoadObject(ofClass: UIImage.self) {
+            self.dealWithImage(result)
+        }
+    }
+    
+    private func dealWithVideo(_ result: PHPickerResult) {
+        clearAll()
+        let movie = UTType.movie.identifier
+        let provider = result.itemProvider
+        
+        provider.loadFileRepresentation(forTypeIdentifier: movie) { url, error in
+            if let url = url {
+                DispatchQueue.main.sync {
+                    let loopType = "com.apple.private.auto-loop-gif"
+                    if provider.hasItemConformingToTypeIdentifier(loopType) {
+                        //                        self.showLoopingMovie(url: url)
+                    } else {
+                        self.showMovie(url: url)
+                    }
+                }
+            }
+        }
+    }
+    
+    private func dealWithImage(_ result: PHPickerResult) {
+        clearAll()
+        let provider = result.itemProvider
+        
+        provider.loadObject(ofClass: UIImage.self) { image, error in
+            if let image = image as? UIImage {
+                DispatchQueue.main.async {
+                    self.showStoryView.image = image
+                }
+            }
+        }
+    }
+    
+    private func showMovie(url: URL) {
+        clearAll()
+        let av = AVPlayerViewController()
+        let player = AVPlayer(url:url)
+        av.player = player
+        self.addChild(av)
+        av.view.frame = self.showStoryView.bounds
+        av.view.backgroundColor = self.showStoryView.backgroundColor
+        self.showStoryView.addSubview(av.view)
+        av.didMove(toParent: self)
+        player.play()
+    }
+    
+    private func clearAll() {
+        if self.children.count > 0 {
+            let avPlayer = self.children[0] as! AVPlayerViewController
+            avPlayer.willMove(toParent: nil)
+            avPlayer.view.removeFromSuperview()
+            avPlayer.removeFromParent()
+        }
+        self.showStoryView.subviews.forEach { $0.removeFromSuperview() }
+    }
+}
