c93f273632617c337ddd42e4753382d8be7639b6
Zoezoe0717 <aaa591129@gmail.com>
2022-11-16 18:29:43 +0800

    add sign page

---
+++ SWiPE/Controllers/Sign/SignUpVC.swift
@@ -0,0 +1,14 @@
+//
+//  SignUpVC.swift
+//  SWiPE
+//
+//  Created by Zoe on 2022/11/16.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
